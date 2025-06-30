3e818a01d18be0c59623968f5e15973cb4cf66cb
Ali Kutlu <kutluali@outlook.com.tr>
2025-06-30 13:24:15 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TasarimTekrar(),
+    );
+  }
+}
+
+class TasarimTekrar extends StatefulWidget {
+  const TasarimTekrar({super.key});
+
+  @override
+  State<TasarimTekrar> createState() => _TasarimTekrarState();
+}
+
+class _TasarimTekrarState extends State<TasarimTekrar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tasarım Tekrar"),
+        backgroundColor: Colors.blue.shade300,
+      ),
+      body: Row(
+        children: [
+          Container(
+            color: Colors.amber,
+            child: Text("merhaba"),
+            decoration: BoxDecoration(border: Border()),
+            //alignment: Alignment.center, // Aligment tüm ekranı kaplar.
+          ),
+        ],
+      ),
+    );
+  }
+}
